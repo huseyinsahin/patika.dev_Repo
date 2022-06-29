7bb6d59e02a7067585f2a7c52be56716501ec7ba
Huseyin Sahin <huseyinsahn@gmail.com>
2022-06-29 15:26:29 +0300

    CalculateGPA project completed.

--- JAVA101/Calculate GPA/CalculateGPA.java
+++ JAVA101/Calculate GPA/CalculateGPA.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 public class CalculateGPA {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int math, physics, chemistry, turkish, history, music, average;
+        int math, physics, chemistry, turkish, history, music;
 
-        System.out.println("Please enter your math grade?");
+        System.out.print("Please enter your math grade : ");
         math = input.nextInt();
         
-        System.out.println("Please enter your physics grade?");
+        System.out.print("Please enter your physics grade : ");
         physics = input.nextInt();
 
-        System.out.println("Please enter your chemistry grade?");
+        System.out.print("Please enter your chemistry grade : ");
         chemistry = input.nextInt();
         
-        System.out.println("Please enter your turkish grade?");
+        System.out.print("Please enter your turkish grade : ");
         turkish = input.nextInt();
 
-        System.out.println("Please enter your history grade?");
+        System.out.print("Please enter your history grade : ");
         history = input.nextInt();
 
-        System.out.println("Please enter your music grade?");
+        System.out.print("Please enter your music grade : ");
         music = input.nextInt();
 
+        double average;
         average = (math + physics + chemistry + turkish + history + music ) / 6;
 
         System.out.println(average > 60 ? "Passed the Class" : "Fail the Class");
