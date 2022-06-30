abd9b6f9ab80810ab797dd14bccc5ee05502ada1
Huseyin Sahin <huseyinsahn@gmail.com>
2022-06-30 09:02:20 +0300

    Area and Perimeter of a Circle project completed.

---
+++ JAVA101/AreaAndPerimeterOfACircle/AreaAndPerimeterOfACircle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AreaAndPerimeterOfACircle {
+    public static void main(String[] args) {
+        //değişkenler buraya gelecek
+        double radius, area, perimeter, circleSliceAngle, circleSliceArea;
+        double pi = 3.14;
+    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter the radius : ");
+        radius = sc.nextDouble();
+
+        System.out.print("Please enter the circle slice angle : ");
+        circleSliceAngle = sc.nextDouble();
+
+        area = pi * radius * radius;
+        perimeter = 2 * pi * radius;
+
+        circleSliceArea = (pi * (radius * radius) * circleSliceAngle) / 360;
+
+        System.out.println("Radius of the circle : " + radius);
+        System.out.println("Angle of circle slice : " + circleSliceAngle);
+        System.out.println("Perimeter of the circle : " + perimeter);
+        System.out.println("Area of the circle : " + area);
+        System.out.println("Area of circle slice : " + circleSliceArea);
+
+        sc.close();
+    }
+}
---
+++ JAVA101/AreaAndPerimeterOfACircle/readme.md
@@ -0,0 +1,10 @@
+# Area and Perimeter of the Circle
+Write a program that calculates the area and perimeter of the circle whose radius you get from the user in Java.
+
+- Area formula : π * r * r;
+- Perimeter formula : 2 * π * r;
+
+Write the program that finds the area of the circle slice whose radius r is the measure of the center angle 𝛼.
+
+- Take 𝜋 = 3.14.
+- Formula : (𝜋 * (r*r) * 𝛼) / 360
