b793b26c9a3c8b8aa37db14a85d709fc63bb6821
Huseyin Sahin <huseyinsahn@gmail.com>
2022-07-01 10:22:08 +0300

    Sorting Numbers project completed.

---
+++ JAVA101/SortingNumbers/SortingNumbers.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SortingNumbers {
+    public static void main(String[] args) {
+
+        int firstNumber, secondNumber, thirdNumber;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter first number : ");
+        firstNumber = input.nextInt();
+
+        System.out.print("Please enter second number : ");
+        secondNumber = input.nextInt();
+
+        System.out.print("Please enter third number : ");
+        thirdNumber = input.nextInt();
+
+        if(firstNumber < secondNumber && firstNumber < thirdNumber){
+            if(secondNumber < thirdNumber){
+                System.out.println(firstNumber + " < " + secondNumber + " < " + thirdNumber);
+            }
+            else{
+                System.out.println(firstNumber + " < " + thirdNumber + " < " + secondNumber);
+            }
+        }else if(secondNumber < firstNumber && secondNumber < thirdNumber){
+            if(firstNumber < thirdNumber){
+                System.out.println(secondNumber + " < " + firstNumber + " < " + thirdNumber);
+            }
+            else{
+                System.out.println(secondNumber + " < " + thirdNumber + " < " + firstNumber);
+            }
+        }else{
+            if(firstNumber < secondNumber){
+                System.out.println(thirdNumber + " < " + firstNumber + " < " + secondNumber);
+            }
+            else{
+                System.out.println(thirdNumber + " < " + secondNumber + " < " + firstNumber);
+            }
+        }
+
+        input.close();
+        
+    }
+}
---
+++ JAVA101/SortingNumbers/readme.md
@@ -0,0 +1,2 @@
+# Sorting Numbers from Small to Large
+Write a program that sorts the 3 entered numbers "from least to greatest".
