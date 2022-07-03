d97b78feccc5bfbe626e3fd56baa09e3c692a2c6
Huseyin Sahin <huseyinsahn@gmail.com>
2022-07-03 20:03:13 +0300

    Diamond with Stars project completed.

--- JAVA101/DiamondWithStars/DiamondWithStars.java
+++ JAVA101/DiamondWithStars/DiamondWithStars.java
@@ -23,12 +23,12 @@ public class DiamondWithStars {
         for (int i = 1; i < n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
-                
             }
+
             for ( int k = 1; k <= ( 2 * n - (2 * i + 1) ); k++) {
                 System.out.print("*");
-                
             }
+            
             System.out.println(" ");
             
          }
