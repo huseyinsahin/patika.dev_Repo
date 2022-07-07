c50539d3a7b58025d505727bd3ef4f21971c34eb
Huseyin Sahin <huseyinsahn@gmail.com>
2022-07-07 08:13:03 +0300

    Exponents Calculator with Recursive project completed.

---
+++ JAVA101/ExponentsCalculatorWithRecursive/ExponentsCalculatorWithRecursive.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ExponentsCalculatorWithRecursive {
+    
+    static int exponentsCalculator(int base, int exponent){
+        
+        if(exponent == 0){
+            return 1;
+        }else{
+            return (base * exponentsCalculator(base, exponent - 1));
+        }
+        
+    }
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the base number : ");
+        int base = input.nextInt();
+        System.out.print("Enter the exponent number : 4");
+        int exponent = input.nextInt();
+
+        System.out.println("Result : "+ exponentsCalculator(base, exponent));
+        input.close();
+    }
+}
